0caf815093c95acf3cbede7f7ac3239826cd4f77
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-05 15:15:31 -0500

    feat(StrictEQ): Add truth table

---
+++ StrictEQ.js
@@ -0,0 +1,21 @@
+// TRUth table (lol) for equality operators
+
+console.log(
+    1 == 1, // true
+    1 == 2, // false
+    1 == '1', // true
+    
+    1 === 1, // true
+    1 === 2, // false
+    1 === '1' // false
+);
+
+console.log(
+    1 != 1, // false
+    1 != 2, // true
+    1 != '1', // false
+
+    1 !== 1, // false
+    1 !== 2, // true
+    1 !== '1' // true
+);
