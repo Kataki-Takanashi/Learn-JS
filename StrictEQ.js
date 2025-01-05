30b730babbdddd79543c6985bd54c1ce8213ec50
Kataki-Takanashi <dev.ali@abdurraheem.com>
2025-01-05 15:34:04 -0500

    docs(StrictEQ): Add explanatory comment on strict comparison operators

--- StrictEQ.js
+++ StrictEQ.js
@@ -19,3 +19,11 @@ console.log(
     1 !== 2, // true
     1 !== '1' // true
 );
+
+/*
+A possible reason for there not being a string less than or equal to operator is that you wouldent know if it failed due to a string or a number.
+For example: if we say <== is our strict less than or equal to operator, then a <== b could be true but we wouldent know if a >== b is false because its less than or because of the type check.
+See the link below for more info: 
+https://stackoverflow.com/questions/14533046/why-doesnt-javascript-have-strict-greater-less-than-comparison-operators
+*/
+
